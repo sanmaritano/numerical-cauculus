29ce4b998c47a0bc355f3688ae5569ba90545171
San Thiago Teixeira <67292685+sanmaritano@users.noreply.github.com>
2021-09-13 21:04:19 -0300

    Update and rename comparison.py to integral-methods/comparison.py

--- integral-methods/comparison.py
+++ integral-methods/comparison.py
@@ -31,33 +31,33 @@ print(f'\nThe exact integral solution is: {exact[0]:.4f}.')
 
 print()
 print(f'\nCOMPARISON BETWEEN TRAPEZOIDAL RULE WITOUTH AND WITH SUBINTERVALS.')
-print(f'\nMétodo do Trapézio = {result_trap:.4f} \
+print(f'\nTrapezoid Rule = {result_trap:.4f} \
     Erro = {erro_trap:.2f}% \
-    Método do Trapézio Repetido = {result_trap_rep:.4f}\
+    Trapezoid Rule with Subinterevals = {result_trap_rep:.4f}\
     Erro = {erro_trap_rep:.2f}% \
     ', end='\t')
 
 print()
 print()
 print(f'\nCOMPARISON WITH SIMPSON 1/3 AND 3/8 METHODS (without subintervals).')
-print(f'\nMétodo Simpson 1/3 = {result_s13:.4f} \
+print(f'\nSimpson 1/3 Rule = {result_s13:.4f} \
     Erro = {erro_s13:.2f}% \
-    Método Simpson 3/8 = {result_s38:.4f}\
+    Simpson 3/8 Rule = {result_s38:.4f}\
     Erro = {erro_s38:.2f}% \
     ', end='\t')
 
 print()
 print()
 print(f'\nCOMPARISON WITH SIMPSON 1/3 AND 3/8 METHODS (with subintervals).')
-print(f'\nMétodo Simpson 1/3 = {result_s13_rep:.4f} \
+print(f'\nSimpson 1/3 Rule = {result_s13_rep:.4f} \
     Erro = {erro_s13_rep:.2f}% \
-    Método Simpson 3/8 = {result_s38_rep:.4f}\
+    Simpson 3/8 Rule = {result_s38_rep:.4f}\
     Erro = {erro_s38_rep:.2f}% \
     ', end='\t')
 
 print()
 print()
 print(f'\nGAUSSIAN QUADRATURE RULE SOLUTION.')
-print(f'\nQuadratura Gaussiana = {result_gauss:.4f}\
+print(f'\nGauss Quadrature = {result_gauss:.4f}\
     Erro = {erro_gauss:.2f}%', end='\t')
 
