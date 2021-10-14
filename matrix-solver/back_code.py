24618a012fc66dfd99af7c066969e40c2ca2df1f
San Thiago Teixeira <jbsanthiago@gmail.com>
2021-10-14 12:03:04 -0300

    version v0.2

--- matrix-solver/back_code.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-import sys
-
-try:
-    import numpy as np
-except ImportError:
-    subprocess.check_call([sys.executable, "-m", "pip", "install", 'numpy'])
-finally:
-    import numpy as np
